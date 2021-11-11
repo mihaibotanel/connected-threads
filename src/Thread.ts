d628f48217b1a18340aaa9dbf8c79e669f540676
mihaibotanel <46228960+mihaibotanel@users.noreply.github.com>
2021-11-11 23:27:12 +0200

    feat: handle files

--- src/Thread.ts
+++ src/Thread.ts
@@ -1,10 +1,12 @@
 import { Worker } from "worker_threads";
+import path from "path";
+import fs from "fs";
 
 interface ArrowFuntion {
-  (...args: any): any
+  (...args: any): any;
 }
 
-export class Thread<TFn extends ArrowFuntion = ArrowFuntion> {
+export class Thread<TFn extends ArrowFuntion> {
   private fn?: TFn;
   private filename?: Readonly<string>;
   private args: Readonly<Array<any>>;
@@ -12,35 +14,47 @@ export class Thread<TFn extends ArrowFuntion = ArrowFuntion> {
   constructor(fn: TFn, args: Parameters<TFn>);
   constructor(filename: string, args: Array<any>);
   constructor(script: TFn | string, args: Array<any>) {
-    if (typeof script === "string") this.filename = script;
-    else this.fn = script;
+    if (typeof script === "string") {
+      if (!fs.existsSync(script))
+        throw new Error(`ENOENT: no such file ${script}`);
+      this.filename = script;
+    } else {
+      this.fn = script;
+    }
     this.args = args;
   }
 
   private getFileWorker(): Worker {
-    return new Worker(this.filename!, {workerData: this.args});
+    const tsFile = path.extname(this.filename!) === ".ts";
+    const script = `
+      if(${tsFile}) require('ts-node').register()
+      require('${this.filename}')
+    `;
+    return new Worker(script, { workerData: this.args, eval: true });
   }
 
   private getScriptWorker(): Worker {
     const script = `
-        (async () => {
-            await ${this.fn!(...this.args)} 
-        })()
+      (async () => {
+          await ${this.fn!(...this.args)} 
+      })()
     `;
     return new Worker(script, { eval: true });
   }
 
   async run() {
     return new Promise((resolve, reject) => {
-        const worker = this.filename ? this.getFileWorker() : this.getScriptWorker();
-        worker.on("online", () => {
-            console.log("online");   
-        })
-        worker.once("message", (data) => {
-            console.log('data' + data)
-        })
-        worker.once("error", reject)
-        worker.once('exit', resolve)
-    })
+      const worker = this.filename
+        ? this.getFileWorker()
+        : this.getScriptWorker();
+      worker.on("online", () => {
+        console.log("online");
+      });
+      worker.once("message", (data) => {
+        console.log("data" + data);
+      });
+      worker.once("error", reject);
+      worker.once("exit", resolve);
+    });
   }
 }
